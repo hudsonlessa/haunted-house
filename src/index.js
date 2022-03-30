6e0ddca00a4e18aa2e879939f364ae7196ef5668
Hudson Lessa <admin@hudsonlessa.com>
2022-03-30 16:52:19 -0300

    Refactor camera positioning after instantiation

--- src/index.js
+++ src/index.js
@@ -36,9 +36,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.x = 3;
-camera.position.y = 3;
-camera.position.z = -3;
+camera.position.set(3, 3, -3);
 camera.lookAt(defaultCube.position);
 scene.add(camera);
 
