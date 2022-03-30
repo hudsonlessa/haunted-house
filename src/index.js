060ad2232bb060aa469efec43076ff02716df75c
Hudson Lessa <admin@hudsonlessa.com>
2022-03-30 12:49:22 -0300

    Instantiate default cube

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './styles.css';
+import * as THREE from 'three';
 
 const { body } = document;
 const canvas = document.createElement('canvas');
 canvas.classList.add('webgl');
 body.appendChild(canvas);
+
+const defaultCubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const defaultCubeMaterial = new THREE.MeshBasicMaterial({ color: '#f00' });
+const defaultCube = new THREE.Mesh(defaultCubeGeometry, defaultCubeMaterial);
