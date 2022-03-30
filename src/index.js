c3ad211c33149e4a7c72566750bf4007389e94a8
Hudson Lessa <admin@hudsonlessa.com>
2022-03-30 12:50:16 -0300

    Add default cube to scene

--- src/index.js
+++ src/index.js
@@ -11,3 +11,4 @@ const scene = new THREE.Scene();
 const defaultCubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const defaultCubeMaterial = new THREE.MeshBasicMaterial({ color: '#f00' });
 const defaultCube = new THREE.Mesh(defaultCubeGeometry, defaultCubeMaterial);
+scene.add(defaultCube);
