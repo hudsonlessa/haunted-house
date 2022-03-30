75d0f59e10a431d96a80d80646d67d1ffd7ae1e9
Hudson Lessa <admin@hudsonlessa.com>
2022-03-30 20:13:14 -0300

    Change directional light name to moonLight

--- src/index.js
+++ src/index.js
@@ -103,13 +103,13 @@ const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.12);
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight('#b9d5ff', 0.12);
-directionalLight.position.set(4, 5, -2);
-gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001);
-gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001);
-gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001);
-gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001);
-scene.add(directionalLight);
+const moonLight = new THREE.DirectionalLight('#b9d5ff', 0.12);
+moonLight.position.set(4, 5, -2);
+gui.add(moonLight, 'intensity').min(0).max(1).step(0.001);
+gui.add(moonLight.position, 'x').min(-5).max(5).step(0.001);
+gui.add(moonLight.position, 'y').min(-5).max(5).step(0.001);
+gui.add(moonLight.position, 'z').min(-5).max(5).step(0.001);
+scene.add(moonLight);
 
 const renderer = new THREE.WebGLRenderer({
   canvas,
