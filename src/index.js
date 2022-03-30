b0c5178f06fc16e136151df213b105211743135e
Hudson Lessa <admin@hudsonlessa.com>
2022-03-30 18:45:00 -0300

    Increase roof position on Y axis

--- src/index.js
+++ src/index.js
@@ -39,6 +39,7 @@ house.add(walls);
 const roofGeometry = new THREE.ConeGeometry(3.5, 1, 4);
 const roofMaterial = new THREE.MeshStandardMaterial({ color: '#b35f45' });
 const roof = new THREE.Mesh(roofGeometry, roofMaterial);
+roof.position.y = 2.5 + 1 / 2;
 house.add(roof);
 
 const camera = new THREE.PerspectiveCamera(
