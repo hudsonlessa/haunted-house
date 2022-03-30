5f7105ffb43f600cbda00769e0f8177f54bf7c8f
Hudson Lessa <admin@hudsonlessa.com>
2022-03-30 12:49:58 -0300

    Instantiate scene

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const canvas = document.createElement('canvas');
 canvas.classList.add('webgl');
 body.appendChild(canvas);
 
+const scene = new THREE.Scene();
+
 const defaultCubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const defaultCubeMaterial = new THREE.MeshBasicMaterial({ color: '#f00' });
 const defaultCube = new THREE.Mesh(defaultCubeGeometry, defaultCubeMaterial);
