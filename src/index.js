5eed553fb0c1432a098f25bbaa30fe36672c7d8d
Hudson Lessa <admin@hudsonlessa.com>
2022-03-31 12:59:19 -0300

    Improve ghost lights

--- src/index.js
+++ src/index.js
@@ -191,19 +191,19 @@ for (let i = 0; i < 50; i += 1) {
   graves.add(grave);
 }
 
-const ghost1 = new THREE.PointLight('#f0f', 2, 3);
+const ghost1 = new THREE.PointLight('#f0f', 1, 20);
 ghost1.castShadow = true;
 ghost1.shadow.mapSize.width = 256;
 ghost1.shadow.mapSize.height = 256;
 ghost1.shadow.camera.far = 7;
 
-const ghost2 = new THREE.PointLight('#0ff', 2, 3);
+const ghost2 = new THREE.PointLight('#0ff', 1, 20);
 ghost2.castShadow = true;
 ghost2.shadow.mapSize.width = 256;
 ghost2.shadow.mapSize.height = 256;
 ghost2.shadow.camera.far = 7;
 
-const ghost3 = new THREE.PointLight('#ff0', 2, 3);
+const ghost3 = new THREE.PointLight('#ff0', 1, 20);
 ghost3.castShadow = true;
 ghost3.shadow.mapSize.width = 256;
 ghost3.shadow.mapSize.height = 256;
